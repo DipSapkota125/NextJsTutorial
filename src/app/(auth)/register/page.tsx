ee4616bd1f2e62d64cfb0861bc381350095446bd
DipSapkota <dipsapkota325@gmail.com>
2025-03-31 17:31:31 +0545

    added nextjs code

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>This is My Register Page</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,9 @@
+const PrivateRoute = () => {
+  return (
+    <div>
+      <h1>This is My Private Route</h1>
+    </div>
+  );
+};
+
+export default PrivateRoute;
